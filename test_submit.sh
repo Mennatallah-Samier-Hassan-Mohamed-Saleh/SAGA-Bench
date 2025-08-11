4724fa8e5a37ffce0aa008db61610972642dee8c
Mennatallah-Samier-Hassan-Mohamed-Saleh <mennaallahsamier97@gmail.com>
2025-08-11 16:30:31 +0400

    Adding a job submission script to test different batch size

---
+++ test_submit.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+#Define the resource requirements here using #SBATCH
+
+#For requesting 128 CPUs
+#SBATCH -c 128
+#SBATCH --exclusive
+#SBATCH --reservation=Thesis_test_Mennatallah
+#Max wallTime for the job
+#SBATCH -t 24:00:00
+
+#Resource requiremenmt commands end here
+
+rm Update.csv
+rm Alg.csv
+#Add the lines for running your code/application
+module purge
+module load gcc cmake perl python libGl libx11 fontconfig mesa
+
+cd /scratch/ms13779/Masters_thesis/Thesis/SAGA-Bench
+
+./frontEnd -d 1 -w 0 -f /home/ms13779/datasets/SAGAdatasets/wiki-topcats.shuffle.t.w.csv -b 16567 -s degAwareRHH -n 1791489 -a bfsdyn -t 128 | tee -a batch_16567.log
