985b972aa45b794d9d29a5e2b32c99e2bf87afc0
Mennatallah-Samier-Hassan-Mohamed-Saleh <mennaallahsamier97@gmail.com>
2025-09-03 18:15:39 +0400

    Addition of scalability test scripts

---
+++ scripts/scalability_run.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+#Clearing any previous csv files
+rm Alg*.csv
+rm Update*.csv 
+
+#Needed Paths
+dataDir=$SCRATCH/datasets/SAGAdatasets/
+sagaDir=$SCRATCH/Masters_thesis/Thesis/SAGA-Bench
+
+STRUCTURES=(degAwareRHH)
+BatchSizes=(
+  200000 250000 300000 375000 400000 500000 600000 750000 1000000 1500000 2000000 3000000
+)
+RUNS=3
+NumThreads=128
+
+# whether each algorithm is weighted or unweighted
+declare -A ALGORITHMS 
+ALGORITHMS=(              
+         [bfsdyn]=0       
+)
+
+# Max num_nodes to initialize for each dataset
+declare -A DATASETS
+DATASETS=(
+       [wiki-topcats.shuffle.t.w.csv]=1791489
+)
+
+cd $sagaDir
+runs=${RUNS}
+while [ $runs -gt 0 ]
+do 
+  for dataset in "${!DATASETS[@]}"; do  
+    for structure in "${STRUCTURES[@]}"; do
+      for algorithm in "${!ALGORITHMS[@]}"; do 
+        for batchSize in "${!BatchSizes[@]}"; do 
+          echo ./frontEnd -d 1 -w ${ALGORITHMS[$algorithm]} -f ${dataDir}$dataset -b ${BatchSizes[batchSize]} -s $structure -n ${DATASETS[$dataset]} -a $algorithm -t ${NumThreads}   
+          ./frontEnd -d 1 -w ${ALGORITHMS[$algorithm]} -f ${dataDir}$dataset -b ${BatchSizes[batchSize]} -s $structure -n ${DATASETS[$dataset]} -a $algorithm -t ${NumThreads}  
+          DIRECTORY=$HOME/SAGA-Bench/scalability/${algorithm}/${structure}/${dataset}/${BatchSizes[batchSize]}/Run${runs}      
+          mkdir -p ${DIRECTORY}
+          mv Alg*.csv ${DIRECTORY}/
+          mv Update*.csv ${DIRECTORY}/
+        done
+      done    
+    done  
+  done 
+runs=$(( $runs - 1 ))
+done 
---
+++ scripts/submit_scalability.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+#Define the resource requirements here using #SBATCH
+
+#For requesting 128 CPUs
+#SBATCH -c 128
+#SBATCH --exclusive
+#SBATCH --reservation=Thesis_test_Mennatallah
+#Max wallTime for the job
+#SBATCH -t 24:00:00
+#SBATCH --output=submit_scalability.out
+#SBATCH --error=submit_scalability.err
+
+# As precaution, clear OMP stuff and remove any alg or update csv files in the folder
+unset OMP_DISPLAY_ENV OMP_NUM_THREADS OMP_PROC_BIND OMP_PLACES
+rm Update.csv
+rm Alg.csv
+
+#Resource requiremenmt commands end here
+module purge
+module load gcc cmake perl python libGl libx11 fontconfig mesa
+
+#Setting up OMP environment variables
+export OMP_DISPLAY_ENV=true
+export OMP_NUM_THREADS=128
+export OMP_PROC_BIND=close
+export OMP_PLACES={0}:128:1
+
+bash $SCRATCH/Masters_thesis/Thesis/SAGA-Bench/scripts/scalability_run.sh
