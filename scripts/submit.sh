2bd5183f6c5e1deac1528149528fccde769cce4a
Mennatallah-Samier-Hassan-Mohamed-Saleh <mennaallahsamier97@gmail.com>
2025-09-03 17:37:51 +0400

    Addition of a submission script using wiki-topcast on BFSdyn algorithm

--- scripts/submit.sh
+++ scripts/submit.sh
@@ -8,14 +8,23 @@
 #SBATCH --reservation=Thesis_test_Mennatallah
 #Max wallTime for the job
 #SBATCH -t 24:00:00
+#SBATCH --output=submit.out
+#SBATCH --error=submit.err
 
-#Resource requiremenmt commands end here
-
+# As precaution, clear OMP stuff and remove any alg or update csv files in the folder
+unset OMP_DISPLAY_ENV OMP_NUM_THREADS OMP_PROC_BIND OMP_PLACES
 rm Update.csv
 rm Alg.csv
-#Add the lines for running your code/application
+
+#Resource requiremenmt commands end here
 module purge
 module load gcc cmake perl python libGl libx11 fontconfig mesa
+
+#Setting up OMP environment variables
+export OMP_DISPLAY_ENV=true
+export OMP_NUM_THREADS=128
+export OMP_PROC_BIND=close
+export OMP_PLACES={0}:128:1
 
 cd $SCRATCH/Masters_thesis/Thesis/SAGA-Bench
 
