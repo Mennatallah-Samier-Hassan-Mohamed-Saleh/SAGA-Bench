43bfc2e1fd78b51e621e33df883e9e3b70961fae
Mennatallah-Samier-Hassan-Mohamed-Saleh <mennaallahsamier97@gmail.com>
2025-10-30 16:27:26 +0400

    Adding an adjlist comparison script

---
+++ scripts/adjlist_test.sh
@@ -0,0 +1,76 @@
+#!/bin/bash
+
+#Define the resource requirements here using #SBATCH
+
+#For requesting 128 CPUs
+#SBATCH -c 128
+#SBATCH --exclusive
+#SBATCH --reservation=Thesis_test_Mennatallah
+#Max wallTime for the job
+#SBATCH -t 24:00:00
+#SBATCH --output=submit.out
+#SBATCH --error=submit.err
+
+# As precaution, clear OMP stuff and remove any alg or update csv files in the folder
+#unset OMP_DISPLAY_ENV OMP_NUM_THREADS OMP_PROC_BIND OMP_PLACES
+rm submit.out
+rm submit.err
+rm Slashdot_adList*
+rm facebook_adList*
+rm test_adList*
+rm Update.csv
+rm Alg.csv
+
+#Setting up OMP environment variables
+export OMP_DISPLAY_ENV=true
+export OMP_NUM_THREADS=1
+#export OMP_PROC_BIND=close
+#export OMP_PLACES={0}:128:1
+
+#Resource requiremenmt commands end here
+module purge
+module load gcc cmake perl python libGl libx11 fontconfig mesa
+
+cd $SCRATCH/Masters_thesis/Thesis/SAGA-Bench
+
+#Testing bfsfromscratch on adList data structures for test.csv
+for (( i=1; i<=5; i++ )); do
+    ./frontEnd -d 1 -w 0 -f ./test.csv -b 10 -s adList -n 40 -a bfsfromscratch -t 1 | tee -a test_adList_scratch_$i.log
+    rm Update.csv
+    rm Alg.csv
+done
+
+#Testing bfsfromscratch on adList data structures for slashdot dataset
+for (( i=1; i<=5; i++ )); do
+    ./frontEnd -d 1 -w 0 -f $SCRATCH/datasets/SAGAdatasets/soc-Slashdot0902.shuffle.t.w.csv -b 16352 -s adList -n 82168 -a bfsfromscratch -t 1 | tee -a Slashdot_adList_scratch_$i.log
+    rm Update.csv
+    rm Alg.csv
+done
+
+#Testing bfsfromscratch on adList data structures for facebook dataset
+for (( i=1; i<=5; i++ )); do
+    ./frontEnd -d 0 -w 0 -f $SCRATCH/datasets/SAGAdatasets/facebook_combined.shuffle.t.w.csv -b 1522 -s adList -n 4039 -a bfsfromscratch -t 1 | tee -a facebook_adList_scratch_$i.log
+    rm Update.csv
+    rm Alg.csv
+done
+
+#Testing bfsdyn on adList data structures for test.csv
+for (( i=1; i<=5; i++ )); do
+    ./frontEnd -d 1 -w 0 -f ./test.csv -b 10 -t 24 -s adList -n 40 -a bfsdyn -t 1 | tee -a test_adList_dyn_$i.log
+    rm Update.csv
+    rm Alg.csv
+done
+
+#Testing bfsdyn on adList data structures for slashdot dataset
+for (( i=1; i<=5; i++ )); do
+    ./frontEnd -d 1 -w 0 -f $SCRATCH/datasets/SAGAdatasets/soc-Slashdot0902.shuffle.t.w.csv -b 16352 -s adList -n 82168 -a bfsdyn -t 1 | tee -a Slashdot_adList_dyn_$i.log
+    rm Update.csv
+    rm Alg.csv
+done
+
+#Testing bfsdyn on adList data structures for facebook dataset
+for (( i=1; i<=5; i++ )); do
+    ./frontEnd -d 0 -w 0 -f $SCRATCH/datasets/SAGAdatasets/facebook_combined.shuffle.t.w.csv -b 1522 -s adList -n 4039 -a bfsdyn -t 1 | tee -a facebook_adList_dyn_$i.log
+    rm Update.csv
+    rm Alg.csv
+done
