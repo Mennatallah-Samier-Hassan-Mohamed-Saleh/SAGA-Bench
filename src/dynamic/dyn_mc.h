6ec4d1240967b1c2aa640c87a1dd260dedb4be7d
Mennatallah-Samier-Hassan-Mohamed-Saleh <mennaallahsamier97@gmail.com>
2025-11-13 10:25:17 +0400

    Minor modification for MC printing

--- src/dynamic/dyn_mc.h
+++ src/dynamic/dyn_mc.h
@@ -119,7 +119,7 @@ void dynMCAlg(T* ds){
     t.Stop();
     //Print the MC for each node 
     for (NodeID n = 0; n < ds->num_nodes; n++) {
-        std::cout << "Node " << n << " : has MC value" << ds->property[n] << std::endl;
+        std::cout << "Node " << n << " : has MC value: " << ds->property[n] << std::endl;
     }     
     ofstream out("Alg.csv", std::ios_base::app);   
     out << t.Seconds() << std::endl;    
@@ -208,7 +208,7 @@ void MCStartFromScratch(T* ds){
     t.Stop();
     //Print the MC for each node 
     for (NodeID n = 0; n < ds->num_nodes; n++) {
-        std::cout << "Node " << n << " : has MC value" << ds->property[n] << std::endl;
+        std::cout << "Node " << n << " : has MC value: " << ds->property[n] << std::endl;
     }       
     ofstream out("Alg.csv", std::ios_base::app);   
     out << t.Seconds() << std::endl;    
