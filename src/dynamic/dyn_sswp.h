5af6100c84e3b4b3ab3a1dbd2901841e9d259d86
Mennatallah-Samier-Hassan-Mohamed-Saleh <mennaallahsamier97@gmail.com>
2025-12-17 14:32:33 +0400

    Commenting printing SSWP values

--- src/dynamic/dyn_sswp.h
+++ src/dynamic/dyn_sswp.h
@@ -135,10 +135,12 @@ void dynSSWPAlg(T* ds, NodeID source){
     }      
 
     t.Stop(); 
+    /*
     // Print the SSWP for each node
     for (NodeID n = 0; n < ds->num_nodes; n++) {
         std::cout << "Node " << n << " : has SSWP value: " << ds->property[n] << std::endl;
-    }       
+    }
+    */       
     ofstream out("Alg.csv", std::ios_base::app);   
     out << t.Seconds() << std::endl;    
     out.close();    
@@ -196,10 +198,12 @@ void SSWPStartFromScratch(T* ds, NodeID source){
     }
 
     t.Stop();
+    /*
     // Print the SSWP for each node
     for (NodeID n = 0; n < ds->num_nodes; n++) {
         std::cout << "Node " << n << " : has SSWP value: " << ds->property[n] << std::endl;
-    }    
+    }
+    */  
     ofstream out("Alg.csv", std::ios_base::app);   
     out << t.Seconds() << std::endl;    
     out.close();
