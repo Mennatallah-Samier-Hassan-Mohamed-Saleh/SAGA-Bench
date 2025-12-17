cef0f216a593e8caec90e523a81947a99ce659ac
Mennatallah-Samier-Hassan-Mohamed-Saleh <mennaallahsamier97@gmail.com>
2025-12-17 14:30:54 +0400

    Commenting printing SSSP values

--- src/dynamic/dyn_sssp.h
+++ src/dynamic/dyn_sssp.h
@@ -117,11 +117,13 @@ void dynSSSPAlg(T* ds, NodeID source){
         ds->affected[i] = false;
     }         
 
-    t.Stop();  
+    t.Stop();
+    /*  
     // Print the SSSP for each node
     for (NodeID n = 0; n < ds->num_nodes; n++) {
         std::cout << "Node " << n << " : has SSSP value: " << ds->property[n] << std::endl;
-    }    
+    }
+    */    
     ofstream out("Alg.csv", std::ios_base::app);   
     out << t.Seconds() << std::endl;    
     out.close();
@@ -217,10 +219,12 @@ void SSSPStartFromScratch(T* ds, NodeID source, float delta){
     }    
 
     t.Stop();
+    /*
     // Print the SSSP for each node
     for (NodeID n = 0; n < ds->num_nodes; n++) {
         std::cout << "Node " << n << " : has SSSP value: " << ds->property[n] << std::endl;
-    }    
+    }
+    */    
     ofstream out("Alg.csv", std::ios_base::app);   
     out << t.Seconds() << std::endl;    
     out.close();
