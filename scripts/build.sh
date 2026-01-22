3390d9a9c11ceef0cb034e65d92c83f16aab73dd
Mennatallah <mennatallah.saleh@nyu.edu>
2026-01-22 16:28:02 +0400

    Update build script directory

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,5 +1,5 @@
 module load gcc cmake perl python libGl libx11 fontconfig mesa
-cd /scratch/ms13779/Masters_thesis/Thesis/SAGA-Bench
+cd /scratch/ms13779/Masters_thesis/Thesis/Integrate_PIGO_SAGA-Bench/SAGA-Bench
 mkdir -p bin obj
 make clean
 make
